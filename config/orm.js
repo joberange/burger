7be53ff35798c63e7ca28906da2ced7057bf98d4
Jobe Range <rangejobe@gmail.com>
2018-08-26 00:28:59 -0700

    cant get the update to work

---
+++ config/orm.js
@@ -0,0 +1,80 @@
+// import in the connection to the database
+var connection = require('../config/connection.js');
+
+// a function that will be used to build queries
+// not necessary but saw someone else using this seems like a good idea for scaling up code later on for numerous queries
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push('?');
+    }
+
+    return arr.toString();
+}
+
+// another function for building queries
+function objToSql(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+        if (ob.hasOwnProperty(key)) {
+            arr.push(key + '=' + ob[key]);
+        }
+    }
+
+    return arr.toString();
+}
+
+// define our orm that will be exported to the burgers.js model
+var orm = {
+    // selectAll function for grabbing everything from the table
+    selectAll: function (tableInput, cb) {
+        var queryString = 'SELECT * FROM ' + tableInput + ';';
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    // insertOne function for inserting one burger into table
+    insertOne: function (table, cols, vals, cb) {
+        var queryString = 'INSERT INTO ' + table;
+        //string builder for query
+        queryString += ' (';
+        queryString += cols.toString();
+        queryString += ') ';
+        queryString += 'VALUES (';
+        // queryString += vals[0] + ' , ' + vals[1];
+        // pushes in question mark for query input sanitation for Values single "?"
+        queryString += printQuestionMarks(vals.length);
+        queryString += ') ';
+
+        console.log(queryString);
+        console.log(vals);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+
+    // update one function for changing a burger status
+    updateOne: function (table, objColVals, condition, cb) {
+        var queryString = 'UPDATE ?? SET ?? AS TRUE WHERE ?? = ??';
+
+        // queryString += ' SET ';
+        // queryString += objToSql(objColVals);
+        // queryString += ' WHERE ';
+        // queryString += condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, [table, devour, id, req.param.id], function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    }// CAN NOT FIGURE OUT PROPER QUERY FOR UPDATE ONE FUNCTION
+};
+
+// export the orm back to the model burger.js
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Everything Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Bang Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Top Flight Burger', true);
